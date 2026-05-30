6cd343236d546b5985db209b1ecdcb41ea85e3bb
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2026-05-30 14:38:35 +0000

    chore(HU-005): adiciona ruff, black e pre-commit

--- apps/acessos/apps.py
+++ apps/acessos/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AcessosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'acessos'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "acessos"
--- config/settings/base.py
+++ config/settings/base.py
@@ -49,12 +49,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": dj_database_url.parse(config("DATABASE_URL"))
-}
+DATABASES = {"default": dj_database_url.parse(config("DATABASE_URL"))}
 
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
