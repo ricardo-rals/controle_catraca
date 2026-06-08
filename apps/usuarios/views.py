51c02c202ca4941c6041b7edc28bdbd10a941e88
Ricardo Augusto <ricardo.rals.dev@gmail.com>
2026-06-08 20:16:35 -0300

    fix: add missing newlines in urls and views files

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 @login_required
-def dashboard( request ):
-    return render( request, "dashboard.html")
+def dashboard(request):
+    return render(request, "dashboard.html")
